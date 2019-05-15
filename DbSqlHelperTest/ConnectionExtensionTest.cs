4d2eacbf88aafab873e026ce526cc3e9107ebd25
ITWeiHan <shps951002@gmail.com>
2019-05-15 15:30:18 +0800

    add ParameterPrefix,QuotePrefix,QuoteSuffix test

--- DbSqlHelperTest/ConnectionExtensionTest.cs
+++ DbSqlHelperTest/ConnectionExtensionTest.cs
@@ -3,19 +3,6 @@ using Xunit;
 
 namespace DbSqlHelperTest
 {
-    public class DbCacheTest : BaseTest
-    {
-        [Fact]
-        public void GetDbCache()
-        {
-            var cache = Db.GetDbCache(); //or "".GetDbCache();
-            Assert.Equal(DBConnectionType.SqlServer, cache.DBConnectionType);
-            Assert.Equal("@", cache.ParameterPrefix);
-            Assert.Equal("[", cache.QuotePrefix);
-            Assert.Equal("]", cache.QuoteSuffix);
-        }
-    }
-
     public class ConnectionExtensionTest : BaseTest
     {
         [Fact]
---
+++ DbSqlHelperTest/DbCacheTest.cs
@@ -0,0 +1,18 @@
+﻿using DbSqlHelper;
+using Xunit;
+
+namespace DbSqlHelperTest
+{
+    public class DbCacheTest : BaseTest
+    {
+        [Fact]
+        public void GetDbCache()
+        {
+            var cache = Db.GetDbCache(); //or "".GetDbCache();
+            Assert.Equal(DBConnectionType.SqlServer, cache.DBConnectionType);
+            Assert.Equal("@", cache.ParameterPrefix);
+            Assert.Equal("[", cache.QuotePrefix);
+            Assert.Equal("]", cache.QuoteSuffix);
+        }
+    }
+}
