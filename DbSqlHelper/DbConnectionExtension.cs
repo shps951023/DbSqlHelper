320a857ed296dbe6a170386efac828a97bbac07b
ITWeiHan <shps951002@gmail.com>
2019-05-26 15:54:37 +0800

    GetDbColumnsSchema method

--- DbSqlHelper/DbConnectionExtension.cs
+++ DbSqlHelper/DbConnectionExtension.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
 
 namespace DbSqlHelper
 {
     public static class DbConnectionExtension
     {
-        private static readonly DBConnectionType DefaultAdapter = DBConnectionType.Unknown;
-        private static readonly Dictionary<string, DBConnectionType> AdapterDictionary
+        private static readonly DBConnectionType _DefaultAdapter = DBConnectionType.Unknown;
+        private static readonly Dictionary<string, DBConnectionType> _AdapterDictionary
                = new Dictionary<string, DBConnectionType>
                {
                    ["oracleconnection"] = DBConnectionType.Oracle,
@@ -24,14 +22,73 @@ namespace DbSqlHelper
         public static DBConnectionType GetDbConnectionType(this Type connectionType)
         {
             var name = connectionType.Name.ToLower();
-            return !AdapterDictionary.ContainsKey(name) ? DefaultAdapter : AdapterDictionary[name];
+            return !_AdapterDictionary.ContainsKey(name) ? _DefaultAdapter : _AdapterDictionary[name];
         }
 
         public static DBConnectionType GetDbConnectionType(this IDbConnection connection)
         {
-            var name = connection.GetType().Name.ToLower();
-            return !AdapterDictionary.ContainsKey(name) ? DefaultAdapter : AdapterDictionary[name];
+            string name = GetDbConnectionTypeLowerName(connection);
+            return !_AdapterDictionary.ContainsKey(name) ? _DefaultAdapter : _AdapterDictionary[name];
         }
+
+        public static string GetDbConnectionTypeLowerName(this IDbConnection connection)
+        {
+            return connection.GetType().Name.ToLower();
+        }
+
+        private static readonly string[] DefaultSchemaKey
+            = new[] { "ColumnName", "ColumnOrdinal", "ColumnSize", "DataType", "AllowDBNull", "DataTypeName", "ProviderSpecificDataType",
+                "IsReadOnly" };
+
+        private static readonly Dictionary<string, string[]> DbColumnsSchemas = new Dictionary<string, string[]> {
+            {"sqlconnection", DefaultSchemaKey },
+            {"sqlceserver", DefaultSchemaKey },
+            {"sqliteconnection", DefaultSchemaKey },
+            {"oracleconnection", new [] {"ColumnName","ColumnOrdinal","ColumnSize","DataType","AllowDBNull","DataTypeName"} },
+            {"mysqlconnection", DefaultSchemaKey },
+            {"npgsqlconnection", DefaultSchemaKey }
+        };
+
+        public static IEnumerable<DbColumnsSchema> GetDbColumnsSchema(this IDbConnection connection,string sql)
+        {
+            connection.OpenCloseConnection();
+            var ds = DbColumnsSchemas[connection.GetDbConnectionTypeLowerName()];
+            using (var command = connection.CreateCommand(sql))
+            {
+                var dt = command.ExecuteReader(CommandBehavior.SchemaOnly).GetSchemaTable();
+                foreach (DataRow s in dt.Rows)
+                {
+                    yield return new DbColumnsSchema
+                    {
+                        ColumnName = s[ds[0]] as string,
+                        ColumnOrdinal = (int)s[ds[1]],
+                        ColumnSize = (int)s[ds[2]],
+                        DataType = s[ds[3]] as Type,
+                        AllowDBNull = (bool)s[ds[4]],
+                        DataTypeName = s[ds[5]] as string,
+                        ProviderSpecificDataType = s[ds[6]] as Type,
+                        IsReadOnly = (bool)s[ds[7]],
+                    };
+                }
+            }
+        }
+
+        public static void OpenCloseConnection(this IDbConnection connection)
+        {
+            if (connection.State == ConnectionState.Closed) connection.Open();
+        }
+    }
+
+    public class DbColumnsSchema
+    {
+        public string ColumnName { get; internal set; }
+        public int ColumnOrdinal { get; internal set; }
+        public int ColumnSize { get; internal set; }
+        public System.Type DataType { get; internal set; }
+        public System.Type ProviderSpecificDataType { get; internal set; }
+        public bool AllowDBNull { get; internal set; }
+        public string DataTypeName { get; internal set; }
+        public bool IsReadOnly { get; internal set; }
     }
 
     public enum DBConnectionType
--- DbSqlHelperTest/ConnectionExtensionTest.cs
+++ DbSqlHelperTest/ConnectionExtensionTest.cs
@@ -1,5 +1,6 @@
 ﻿using DbSqlHelper;
 using Xunit;
+using System.Linq;
 
 namespace DbSqlHelperTest
 {
@@ -17,6 +18,23 @@ namespace DbSqlHelperTest
             {
                 var result = typeof(System.Data.SqlClient.SqlConnection).GetDbConnectionType();
                 Assert.Equal(DBConnectionType.SqlServer, result);
+            }
+        }
+
+        [Fact]
+        public void GetDbColumnsSchema()
+        {
+            using (var cn = Db.GetConnection())
+            {
+                var sql = "select 1 id,'test' val,null nullCol";
+
+                var result = cn.GetDbColumnsSchema(sql).ToArray();
+
+                Assert.Equal("id", result[0].ColumnName);
+                Assert.Equal(typeof(int), result[0].DataType);
+                Assert.Equal(typeof(string), result[1].DataType);
+
+                Assert.True(result[2].AllowDBNull);
             }
         }
     }
